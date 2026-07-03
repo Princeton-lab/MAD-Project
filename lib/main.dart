7c135033893bf05736b57b6c38c3a09abf6c7e90
Princeton-lab <PRINCETONONG.24@ichat.sp.edu.sg>
2026-07-04 01:33:10 +0800

    Added admin, alert, edit, log and profile screens

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:project/homescreen.dart';     
 import 'package:project/stockscreen.dart';
 import 'package:project/itemdetailscreen.dart';
-import 'package:project/add/editscreen.dart';
-import 'package:project/add/adminscreen.dart';
-import 'package:project/add/alertscreen.dart';
-import 'package:project/add/logscreen.dart';
-import 'package:project/add/profilescreen.dart';
+import 'package:project/editscreen.dart';
+import 'package:project/adminscreen.dart';
+import 'package:project/alertscreen.dart';
+import 'package:project/logscreen.dart';
+import 'package:project/profilescreen.dart';
 
 void main() {
   runApp( MaterialApp(
