61efc199395864035c8e2484e3915cbc0c6d7d0b
Princeton-lab <PRINCETONONG.24@ichat.sp.edu.sg>
2026-07-23 10:17:16 +0800

    update admin,home,login

--- lib/adminscreen.dart
+++ lib/adminscreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 class AdminScreen extends StatefulWidget {
   const AdminScreen({super.key});
 
@@ -11,6 +11,407 @@ class _AdminScreenState extends State<AdminScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body:SafeArea(
+  child:Padding(
+    padding:const EdgeInsets.all(20),
+    child:ListView(
+
+      children:[
+        Row(
+          mainAxisAlignment:MainAxisAlignment.spaceBetween,
+        children:[
+          const Text('DASHBOARD',
+          style:TextStyle(
+            fontSize:24,
+            fontWeight:FontWeight.bold
+            ),
+          ),
+          IconButton(
+            onPressed:(){},
+            icon :const Icon(Icons.notifications_none),
+          ),
+   
+          
+        ],
+        ),
+               const SizedBox(height:15 ),
+          const Align(alignment: Alignment.centerLeft,
+          child: Text('Hello John!',//will be changed to the name of the user logged in
+          style:TextStyle(
+            fontSize:20,
+            fontWeight:FontWeight.bold
+            ),
+          ),
+          ),
+        const SizedBox(height:5),
+        const Align(alignment: Alignment.centerLeft,
+          child: Text('Here`s your inventory overview',
+          style:TextStyle(
+           
+            color:Colors.grey
+            ),
+          ),
+          ),
+    
+      
+       
+  Container(
+  padding: const EdgeInsets.all(20),
+  width: double.infinity,
+  decoration: BoxDecoration(
+    border: Border.all(color: Colors.black),
+    borderRadius: BorderRadius.circular(12),
+  ),
+
+  child: Row(
+    
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+     
+
+      // LEFT SIDE
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          Text(
+            "Total Items",
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+
+          SizedBox(height: 8),
+
+          StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+          .collection('Items')
+          .snapshots(),
+  builder: (context, snapshot) {
+
+    if (!snapshot.hasData) {
+      return const Text("0");
+    }
+
+    final items = snapshot.data!.docs;
+
+    return Text(
+      items.length.toString(),
+      style: const TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  },
+),
+
+        ],
+      ),
+
+      // RIGHT SIDE
+      Container(padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(12),
+        
+      ),
+     child:IconButton(
+          color: Colors.purple,
+          tooltip: 'Open stock screen',
+          icon: const Icon(Icons.inventory_2),
+       onPressed: () {
+        Navigator.pushNamed(context, '/stockpage');
+       },
+      ),
+  )],
+  ),
+),
+const SizedBox(height: 20),
+  
+       
+  Container(
+  padding: const EdgeInsets.all(20),
+  width: double.infinity,
+  decoration: BoxDecoration(
+  border: Border.all(color: Colors.black),
+    
+    borderRadius: BorderRadius.circular(12),
+  ),
+
+  child: Row(
+    
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+     
+
+      // LEFT SIDE
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          Text(
+            "Low Stock Items",
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+
+          SizedBox(height: 8),
+        StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+          .collection('Items')
+          .snapshots(),
+  builder: (context, snapshot) {
+
+    if (!snapshot.hasData) {
+      return const Text("0");
+    }
+
+    final items = snapshot.data!.docs;
+    final lowStockItems = items.where((item) {
+      return item['Quantity'] <= item['Minimum Stock'];
+    }).toList();
+    
+    return Text(
+      lowStockItems.length.toString(),
+      style: const TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  },
+),
+
+
+        ],
+      ),
+
+      // RIGHT SIDE
+      Container(padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(12),
+        
+      ),
+     child:IconButton(
+          color: Colors.red,
+          tooltip: 'Open low item screen',
+          icon: const Icon(Icons.warning_amber_rounded),
+       onPressed: () {
+        Navigator.pushNamed(context, '/alertpage');
+       },
+      ),
+  )],
+  ),
+),
+SizedBox(height: 20),
+
+ Container(
+  padding: const EdgeInsets.all(20),
+  width: double.infinity,
+  decoration: BoxDecoration(
+  border: Border.all(color: Colors.black),
+    
+    borderRadius: BorderRadius.circular(12),
+  ),
+
+  child: Row(
+    
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+     
+
+      // LEFT SIDE
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          Text(
+            "Total Stock Value",
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+
+          SizedBox(height: 8),
+
+          StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+          .collection('Items')
+          .snapshots(),
+  builder: (context, snapshot) {
+
+    if (!snapshot.hasData) {
+      return const Text("0");
+    }
+
+    final items = snapshot.data!.docs;
+    double totalValue = 0;
+
+    for (var item in items) {
+      totalValue +=
+          (item['Price'] as num).toDouble() *
+          (item['Quantity'] as num).toDouble();
+    }
+    return Text(
+      "\$${totalValue.toStringAsFixed(2)}",
+      style: const TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  },
+),
+
+        ],
+      ),
+
+      // RIGHT SIDE
+      Container(padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(12),
+        
+      ),
+     child:IconButton(
+          color: Colors.green,
+          tooltip: 'Open total stock value screen',
+          icon: const Icon(Icons.attach_money),
+       onPressed: () {
+        Navigator.pushNamed(context, '/stockpage');
+       },
+      ),
+  )],
+  ),
+),
+SizedBox(height: 20),
+
+ Container(
+  padding: const EdgeInsets.all(20),
+  width: double.infinity,
+  decoration: BoxDecoration(
+  border: Border.all(color: Colors.black),
+    
+    borderRadius: BorderRadius.circular(12),
+  ),
+
+  child: Row(
+    
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+     
+
+      // LEFT SIDE
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          Text(
+            "Recent activities",
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+
+          SizedBox(height: 8),
+
+          StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+          .collection('ActivityLog')
+          .snapshots(),
+  builder: (context, snapshot) {
+
+    if (!snapshot.hasData) {
+      return const Text("0");
+    }
+
+    final log = snapshot.data!.docs;
+
+    return Text(
+      log.length.toString(),
+      style: const TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  },
+),
+
+        ],
+      ),
+
+      // RIGHT SIDE
+      Container(padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(12),
+        
+      ),
+     child:IconButton(
+          color: Colors.blue,
+          tooltip: 'Open recent activities',
+          icon: const Icon(Icons.calendar_month),
+       onPressed: () {
+        Navigator.pushNamed(context, '/logpage');
+       },
+      ),
+  )],
+  ),
+  ),
+SizedBox(height: 20),
+
+ Container(
+  padding: const EdgeInsets.all(20),
+  width: double.infinity,
+  decoration: BoxDecoration(
+  border: Border.all(color: Colors.black),
+    
+    borderRadius: BorderRadius.circular(12),
+  ),
+
+  child: Row(
+    
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+     
+
+      // LEFT SIDE
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          Text(
+            "Registration",
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+
+      // RIGHT SIDE
+      Container(padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(12),
+        
+      ),
+     child:IconButton(
+          color: Colors.blue,
+          tooltip: 'Register',
+          icon: const Icon(Icons.person_add),
+       onPressed: () {
+        Navigator.pushNamed(context, '/register');
+       },
+      ),
+  )],
+  ),
+)
+      ],
+    ),
+  
+  ),
+),
       bottomNavigationBar: Container(
     padding: const EdgeInsets.all(15),
     color: const Color.fromARGB(255, 185, 176, 202),
@@ -28,10 +429,10 @@ class _AdminScreenState extends State<AdminScreen> {
        SizedBox(height: 20),
        IconButton(
           color: Colors.white,
-          tooltip: 'Open item details',
+          tooltip: 'Open stock page',
           icon: const Icon(Icons.list_alt),
        onPressed: () {
-        Navigator.pushNamed(context, '/itemdetails');
+        Navigator.pushNamed(context, '/stockpage');
        },
       ),
       SizedBox(height: 20),
